9d13352b663cf3b48694f0f3b956e3bff06030da
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-20 10:00:55 -0400

    adding display info method to Book obj

--- gammaExamPt1.js
+++ gammaExamPt1.js
@@ -9,5 +9,14 @@ function Book(title, author, genre, length, isCheckedIn){
 	this.genre = genre;
 	this.length = length;
 	this.isCheckedIn = isCheckedIn;
-	
+
+	this.displayAllInfo = function(){
+		console.log("\nBook Info\n----------------------\nTitle: %s\nAuthor: %s\nGenre: %s\nNumber of pages: %s\nChecked in: %s",this.title, this.author, this.genre, this.length, this.isCheckedIn)
+	}
 }
+
+
+
+var javaScriptForDummies = new Book("JavaScript For Dummies","Mr. Dummie","non-fiction", 342, true);
+
+javaScriptForDummies.displayAllInfo();
